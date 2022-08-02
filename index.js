e42ade8744022097df0f923a634103b3a0bc9048
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-02 23:04:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+shout('Hello');
+// First
+function whisper(string) {
+    return string.toLowerCase();
+}
+whisper("HELLO");
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+logShout("hello");
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper("HELLO");
+
+function sayHiToHeadphonedRoommate(string) {
+    var isLowerCase = 'hello';
+    isLowerCase.toLowerCase();
+    var isUpperCase = 'HELLO';
+    isUpperCase.toUpperCase();
+    if (string === isLowerCase) {
+
+        return "I can't hear you!";
+    } else if (string === isUpperCase) {
+        return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+    // else {
+    //     return 0;
+    // }
+}
+// console.log(sayHiToHeadphonedRoommate("hello"));
+// console.log(sayHiToHeadphonedRoommate("HELLO"));
+// console.log(sayHiToHeadphonedRoommate("Let's have dinner together!"));
+sayHiToHeadPhonedRoommate('hello');
+sayHiToHeadPhonedRoommate("HELLO");
+sayHiToHeadPhonedRoommate("Let's have dinner together!");
